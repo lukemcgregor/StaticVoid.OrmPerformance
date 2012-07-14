cb1941fe10959b68d1fc3a4e61499d07692c7121
Eli Weinstock-Herman <tarwn@tiernok.com>
2012-07-13 23:16:53 -0400

    Additional petapoco tests

--- Harness.PetaPoco/PetaPocoConfiguration.cs
+++ Harness.PetaPoco/PetaPocoConfiguration.cs
@@ -7,8 +7,9 @@ using PetaPoco;
 using StaticVoid.OrmPerformance.Harness.Contract;
 using StaticVoid.OrmPerformance.Harness.Models;
 
+
 namespace StaticVoid.OrmPerformance.Harness.PetaPoco {
-	public class PetaPocoConfiguration : IRunnableInsertConfiguration, IRunnableUpdateConfiguration {
+	public class PetaPocoConfiguration : IRunnableInsertConfiguration, IRunnableUpdateConfiguration, IRunnableDeleteConfiguration, IRunnableDiscreteSelectConfiguration, IRunnableBulkSelectConfiguration {
 		public string Name { get { return "Basic"; } }
 
 		public string Technology { get { return "PetaPoco 4.03"; } }
@@ -34,10 +35,23 @@ namespace StaticVoid.OrmPerformance.Harness.PetaPoco {
 			};
 			_db.Update("TestEntities", "Id", entity);
 		}
+		
+		public void Delete(int id) {
+			_db.Delete("TestEntities","Id", new { Id = id });
+		}
+
+		public TestEntity Find(int id) {
+			return _db.Single<TestEntity>("SELECT Id, TestInt, TestDate, TestString FROM TestEntities WHERE Id = @0", id);
+		}
+
+		public IEnumerable<TestEntity> FindWhereTestIntIs(int testInt) {
+			return _db.Query<TestEntity>("SELECT Id, TestInt, TestDate, TestString FROM TestEntities WHERE TestInt = @0", testInt);
+		}
 
 		public void Commit() {		}
 		public void TearDown() {
 			_db.Dispose();
 		}
+
 	}
 }
