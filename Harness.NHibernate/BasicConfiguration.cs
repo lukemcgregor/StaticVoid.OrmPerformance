f048be501c974d9bcaa103a38664115d686548bd
Eli Weinstock-Herman <tarwn@tiernok.com>
2012-07-16 22:09:44 -0400

    Addition of 3 nhibernate insert tests (all currently failing assertions?)

--- Harness.NHibernate/BasicConfiguration.cs
+++ Harness.NHibernate/BasicConfiguration.cs
@@ -4,47 +4,49 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NHibernate;
+using NHibernate.Bytecode;
 using NHibernate.Cfg;
+using NHibernate.Dialect;
 using StaticVoid.OrmPerformance.Harness.Contract;
 
-namespace StaticVoid.OrmPerformance.Harness.NHibernate
-{
-    public class BasicConfiguration: 
-        IRunnableInsertConfiguration
-    {
-        public string Name { get { return "Basic Configuration"; } }
-        public string Technology { get { return "NHibernate"; } }
+namespace StaticVoid.OrmPerformance.Harness.NHibernate {
+	public class BasicConfiguration : IRunnableInsertConfiguration {
+		public string Name { get { return "Basic Configuration"; } }
+		public string Technology { get { return "NHibernate"; } }
 
-        private ISessionFactory _sessionFactory;
-        private Configuration _configuration;
-        private IStatelessSession _session;
-        private ITransaction _transaction;
+		private ISessionFactory _sessionFactory;
+		private Configuration _configuration;
+		private ISession _session;
+		private ITransaction _transaction;
 
-        public void Add(Models.TestEntity entity)
-        {
-            _session.Insert(entity);
-        }
+		public BasicConfiguration(IConnectionString connectionString) {
+			_configuration = new Configuration();
+			_configuration.Proxy(p => p.ProxyFactoryFactory<DefaultProxyFactoryFactory>())
+						  .DataBaseIntegration(db => {
+							  db.ConnectionString = connectionString.FormattedConnectionString;
+							  db.Dialect<MsSql2008Dialect>();
+						  })
+						  .AddAssembly(typeof(NHTestEntity).Assembly);
+		}
 
-        public void Setup()
-        {
-            //TODO: configuration needs to include mappings and an appropriate batch size
-            _configuration = new Configuration();
-            _configuration.Configure();
-            _sessionFactory = _configuration.BuildSessionFactory();
-            _session = _sessionFactory.OpenStatelessSession();
-            _transaction = _session.BeginTransaction();
-        }
+		public void Setup() {
+			_sessionFactory = _configuration.BuildSessionFactory();
+			_session = _sessionFactory.OpenSession();
+			_transaction = _session.BeginTransaction();
+		}
 
-        public void TearDown()
-        {
-            _transaction.Dispose();
-            _session.Dispose();
-            _sessionFactory.Dispose();
-        }
+		public void Add(Models.TestEntity entity) {
+			_session.Save(new NHTestEntity(entity));
+		}
 
-        public void Commit()
-        {
-            _transaction.Commit();
-        }
-    }
+		public void Commit() {
+			_transaction.Commit();
+		}
+
+		public void TearDown() {
+			_transaction.Dispose();
+			_session.Dispose();
+			_sessionFactory.Dispose();
+		}
+	}
 }
---
+++ Harness.NHibernate/NHTestEntity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StaticVoid.OrmPerformance.Harness.Models;
+
+namespace StaticVoid.OrmPerformance.Harness.NHibernate {
+	public class NHTestEntity {
+
+		public virtual int Id { get; protected set;}
+		public virtual int TestInt { get; set; }
+		public virtual string TestString { get; set; }
+		public virtual DateTime TestDate { get; set; }
+
+		public NHTestEntity() { }
+		public NHTestEntity(TestEntity entity) {
+			Id = entity.Id;
+			TestDate = entity.TestDate;
+			TestString = entity.TestString;
+			TestInt = entity.TestInt;
+		}
+	}
+
+	
+}
