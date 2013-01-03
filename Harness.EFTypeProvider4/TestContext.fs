c8a75d713f570dd0f80c5dcb469d3a99a1b014a7
Joel Mueller <joel.mueller@gmail.com>
2013-01-03 09:44:46 -0600

    Adding test harness for the F# Entity Framework Type Provider

---
+++ Harness.EFTypeProvider4/TestContext.fs
@@ -0,0 +1,15 @@
+﻿namespace StaticVoid.OrmPerformance.Harness.EFTypeProvider4
+
+open System.Data.Linq
+open System.Data.EntityClient
+open Microsoft.FSharp.Data.TypeProviders
+open StaticVoid.OrmPerformance.Harness.Contract
+
+type internal TestDb = SqlEntityConnection<ConnectionStringName="TestDB", Pluralize=true>
+
+[<AutoOpen>]
+module internal DbExtensions =
+
+    type TestDb with
+        static member GetConfiguredContext(cnstr:IConnectionString) =
+            TestDb.GetDataContext(cnstr.FormattedConnectionString + "MultipleActiveResultSets=true;")
